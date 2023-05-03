e678ec1b641d5861c199894118c3633811dee807
NicolasRodriguezSteuerberg <nrodriguezsteuerberg@danielcastelao.org>
2023-05-03 14:25:57 +0200

    tomalo

--- src/View.java
+++ src/View.java
@@ -1,6 +1,5 @@
 public class View {
     void muestraVelocidad(String matricula, Integer v){
         Dialog.vDialogo("El coche " + matricula + " tiene una velocidad de: " + v + "km/h");
-
     }
 }
