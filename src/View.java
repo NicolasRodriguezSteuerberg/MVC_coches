4c0378f8b71c86a720f092cb8a242fe8babb4f8e
NicolasRodriguezSteuerberg <nrodriguezsteuerberg@danielcastelao.org>
2023-05-10 14:22:50 +0200

    Observer

--- src/View.java
+++ src/View.java
@@ -1,4 +1,8 @@
 public class View {
+    /**
+     * @param matricula
+     * @param v
+     */
     void muestraVelocidad(String matricula, Integer v){
         Dialog.vDialogo("El coche " + matricula + " tiene una velocidad de: " + v + "km/h");
     }
