630bb4782885e3e6ad1e9218c3a72e1b5375502f
NicolasRodriguezSteuerberg <nrodriguezsteuerberg@danielcastelao.org>
2023-05-19 09:42:49 +0200

    observer

--- src/OVelocidad.java
+++ src/OVelocidad.java
@@ -3,6 +3,13 @@ import java.util.Observer;
 
 public class OVelocidad implements Observer {
     View miVista = new View();
+
+    /**
+     * oberva cuando cambia la velocidad
+     * @param o     the observable object.
+     * @param arg   an argument passed to the {@code notifyObservers}
+     *                 method.
+     */
     @Override
     public void update(Observable o, Object arg) {
         //Precisamos castear el objeto a Coche
